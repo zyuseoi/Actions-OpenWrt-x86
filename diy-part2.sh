7af34c8df2c53e684c9cf2d3862ea761d386d490
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-04-06 21:49:24 +0800

    Delete diy-part2.sh

--- diy-part2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 自定义后台地址
-sed -i 's#192.168.1.1#192.168.2.101#g' package/base-files/files/bin/config_generate
-# 自定义取消密码
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
