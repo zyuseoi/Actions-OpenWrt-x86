e9db00e5864dfc974c97e7cc5a3d14a07c5c4b5d
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-04-06 21:49:09 +0800

    Update and rename diy-part1.sh to diy.sh

--- diy.sh
+++ diy.sh
@@ -3,10 +3,21 @@
 # 取消对提要源的注释
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-cd openwrt
+# 更新安装源
+cd openwrt && ./scripts/feeds update -a
+cd openwrt && ./scripts/feeds install -a
 
 # 添加源
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+
+# 更新安装源
+cd openwrt && ./scripts/feeds update -a
+cd openwrt && ./scripts/feeds install -a
+
+# 自定义后台地址
+sed -i 's#192.168.1.1#192.168.2.101#g' package/base-files/files/bin/config_generate
+# 自定义取消密码
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 自定义配置文件
 rm -f ./.config*
