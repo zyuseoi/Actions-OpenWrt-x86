a356d629876297077d372ffa22c6f232cea126de
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-09 23:24:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -48,8 +48,8 @@ EOF
 
 # IPv6支持
 # cat >> .config <<EOF
-# CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
-# CONFIG_PACKAGE_ipv6helper=y
+# CONFIG_PACKAGE_dnsmasq_full_dhcpv6 is not set
+# CONFIG_PACKAGE_ipv6helper is not set
 # EOF
 
 # 多文件系统支持
