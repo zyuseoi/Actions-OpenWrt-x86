648dfae6f4c088a95a09bb10ed73e462d171449b
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-10 19:21:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
+# 取消对提要源的注释
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/small-5/luci-app-adblock-plus package/luci-app-adblock-plus
+# 添加源
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+
