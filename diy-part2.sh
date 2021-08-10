54e06ad930bdb2486360405cdf9907fe9ac1d24d
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-10 19:20:16 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 F-T-Otaku
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/F-T-Otaku/Actions-ImmortalWrt-R1S_H5
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
