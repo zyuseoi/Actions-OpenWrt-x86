5cf6e851625ccfcd67021d49ab5468aefa4ffdf5
zyuseoi <67495236+zyuseoi@users.noreply.github.com>
2021-08-10 19:22:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+# 自定义后台地址
+sed -i 's#192.168.1.1#192.168.2.101#g' package/base-files/files/bin/config_generate
+# 自定义取消密码
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
